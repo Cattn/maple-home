43336369823b35d4786111e3f2934a4d490bb293
Logan <Logan@cattn.dev>
2025-06-19 22:17:18 -0400

    Initial Push

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+
+	import { onMount } from 'svelte';
+
+	let isMobile = false;
+	onMount(() => {
+		isMobile = window.innerWidth <= 768;
+	});
+
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Button, Icon } from "m3-svelte";
+    /* import Face from '@material-design-icons/svg/filled/face.svg?raw'; */
+    import iconSettings from "@ktibow/iconset-material-symbols/settings-outline";
+
+</script>
+
+<Button square={false} iconType="left" variant="filled">
+    <Icon icon={iconSettings} />
+    Hello
+</Button>
