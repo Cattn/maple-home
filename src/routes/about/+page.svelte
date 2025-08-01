c16f2561812172ecc3e7df8e75857ed515d6bba9
Logan <Logan@cattn.dev>
2025-07-31 21:14:19 -0400

    fix styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,7 +12,7 @@
 </div>
 
 <div class="flex justify-center px-4 md:px-0">
-    <div class="flex space-x-1">
+    <div class="flex space-x-1 mx-2 md:mx-32">
         <h1 class="text-wrap text-center text-secondary text-base md:text-xl">
             Maple is a music library and organization tool. It allows you to upload your music library and watch it be organized into albums, artists, playlists, and more! You are given robust controls to play and organize your music. It also includes server-side & social features, such as discord RPC integration, friends, statuses, and in the future: playlist transfers & other P2P functions. All music handled client side.
         </h1>
